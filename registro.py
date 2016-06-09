318d028eba8acd025bee8939d3d4df701d63fcd8
kenneth2016 <kennethl1801@gmail.com>
2016-06-09 08:47:26 -0600

    solucion registro

--- registro.py
+++ registro.py
@@ -1,11 +1,32 @@
-opc= int(input("elija una opcion: \n 1.crear estudiante \n 2.ingresar notas \n 3.reporte de notas \n 4.salir \n: "))
-students={}
-while opc!=4:
+# autor : Kenneth Lopez
+print("1.crear estudiante \n 2.ingresar notas \n 3.reporte de notas \n 4. salir")
+opc=int(input("ingrese que opcion quiere :  "))
+estudiantes = {}
+while opc != 4:
 	if opc == 1:
-		nom=input("ingrese el nombre del estudiante: ")
-		students[nom]=[]
-		print (students)
-	opc= int(input("elija una opcion: \n 1.crear estudiante \n 2.ingresar notas \n 3.reporte de notas \n 4.salir \n: "))
+		n=input("ingrese el estudiante: ")
+		estudiantes[n]=[]
+		print (estudiantes)
+		print("1.crear estudiante \n 2.ingresar notas \n 3.reporte de notas \n 4. salir")
+		opc=int(input("ingrese que opcion quiere :  "))	
 	if opc == 2:
-		estu = input("a que estudiante quiere agregar una nota: ")
-		nota int(input("ingrese la nota: "))
+		estu=input (" a que estudiante le quiere ingresar la nota:  ")
+		opc2 = "si"
+		while opc2 == "si":
+			nota=int(input("ingrese la nota: "))
+			while nota <= 0 or nota >= 101:
+				nota=int( input("ingrese la nota: ") )
+			estudiantes[estu].append(nota)
+			print (estudiantes)
+			opc2=input("quiere ingresar otra nota al mismo estudiante  :  ")
+		print("1.crear estudiante \n 2.ingresar notas \n 3.reporte de notas \n 4. salir")
+		opc=int(input("ingrese que opcion quiere :  "))
+	if opc == 3: 
+		estu=input (" el reporte de que estudiante quiere ver:  ")	
+		prom = sum(estudiantes[estu])/len(estudiantes[estu])
+		print (estudiantes[estu] )
+		print(prom)
+		print("1.crear estudiante \n 2.ingresar notas \n 3.reporte de notas \n 4. salir")
+		opc=int(input("ingrese que opcion quiere :  "))	
+	
+print ("gracias")
