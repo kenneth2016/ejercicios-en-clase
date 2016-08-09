7adc362937373fe435232b034ea9fa9744fd1e29
kenneth2016 <kennethl1801@gmail.com>
2016-08-09 08:49:28 -0600

    clases de figuras

---
+++ figura.py
@@ -0,0 +1,10 @@
+class FiguraGeometrica():
+	def __init__(self, base, altura):
+		self.base = base
+		self.altura = altura
+
+	def imprimir (self):
+		return ""
+
+	def calcular_area(self):
+		return self.base * self.altura 
