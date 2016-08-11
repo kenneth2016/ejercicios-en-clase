dc2e0f0603bbed6105a6555a189d8bbd8a5f6046
kenneth2016 <kennethl1801@gmail.com>
2016-08-11 08:50:17 -0600

    casi terminado

--- figuras.py
+++ figuras.py
@@ -4,9 +4,30 @@ from triangulo import Triangulo
 figura =None
 print ("1.crear figura \n 2.salir")
 
-opc=int(input("ingrese una opcion: "))
+opc1=int(input("ingrese una opcion: "))
+opc2=None
 
-while opc!= 2:
-	if opc==1:
-		fig= int(input("1.cuadrado , 2.triangulo: "))
-		
+while opc2!=4:
+	if opc1==1 or opc2==2:
+		fig=int(input(" elija una figura: \n 1. cuadrado \n 2. triangulo: "))
+		if fig == 2:
+			altura= int(input("ingrese la altura: "))
+			base = int(input("ingrese la base: "))
+			figura=Triangulo (base, altura)
+
+		if fig == 1:
+			lado = int(input("ingrese el lado: "))
+			figura= Cuadrado(lado)
+
+	if opc2==2:
+		print("area: " , figura.calcular_area())
+
+	if opc2==3:
+		print ("figura: ", figura.imprimir())
+
+	print("1.crear figura\n 2.mostrar area\n 3.imprimir\n 4.salir")
+	opc2=int(input("elija la opcion que desea: "))
+
+
+
+
