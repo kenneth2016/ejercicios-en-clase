f267b93d94fdf1a946a83c2b081121f0091acd32
kenneth2016 <kennethl1801@gmail.com>
2017-01-18 08:28:18 -0600

    ejercicio rectangulo

---
+++ prueba_rectangulo.py
@@ -0,0 +1,6 @@
+from rectangulo import rectangulo
+
+base = int(input("ingrese la base: "))
+altura = int(input("ingrese la altura: "))
+
+print (rectangulo(base, altura))
---
+++ rectangulo.py
@@ -0,0 +1,6 @@
+def rectangulo (altura, base):
+	p =""
+	for i in range(0, base):
+		p += ("* "*altura)+"\n"
+	return p
+	
