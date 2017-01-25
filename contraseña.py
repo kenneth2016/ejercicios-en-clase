98106ae093ba411b897f72b1da19afa39702b1db
kenneth2016 <kennethl1801@gmail.com>
2017-01-25 08:51:10 -0600

    avance contraseña

---
+++ contraseña.py
@@ -0,0 +1,6 @@
+print ("bienvenido al creador de contraseñas")
+letras =["a", "b", "c", "d", "e", "f", "g", "h","i", "j", "k", "l","m","n", "o", "p", "q", "r", "s", "t","u", "v", "w", "x","y","z", "A", "B", "C", "D", "E", "F","G", "H", "I", "J","K","L", "M", "N", "O", "P", "Q", "R","S", "T", "U", "V","W","X", "Y", "Z", "1", "2", "3", "4","5", "6", "7", "8","9","0"]
+cant= int(input("ingrese la cantidad de contraseñas: "))
+longi= int(input("ingrese la longitud de sus contraseñas: "))
+
+
---
+++ prueba_contraseña.py
@@ -0,0 +1,7 @@
+import random
+def crear_archivo(password):
+	file = open("archivo.txt", "r")
+	file.close()
+
+def crear_contra(cant, longi, letras):
+	
