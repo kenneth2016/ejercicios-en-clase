4b5cd3c66888f1c321eeb5a0692d95f913347314
kenneth2016 <kennethl1801@gmail.com>
2017-01-27 08:51:13 -0600

    casi

--- prueba_contraseña.py
+++ prueba_contraseña.py
@@ -1,7 +1,16 @@
 import random
 def crear_archivo(password):
-	file = open("archivo.txt", "r")
+	file = open("archivo.txt", "w")
 	file.close()
 
 def crear_contra(cant, longi, letras):
-	
+	contraseñas= []
+
+	for i in range(cant):
+		contra_final= []
+		random.shuffle(letras)
+		for i in range(longi):
+			contra_final.append(letras[i])
+			contraseñas.append(contra_final)
+
+	return contraseñas
